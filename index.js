dd337540712306ebe290bd0726d938692477c684
Ldn013 <contato.leonardoclaro@gmail.com>
2023-10-24 17:06:54 -0300

    Meu segundo Commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log ("É nois que banca")
+// Dados de produtos (exemplo)
+const products = [
+    'Produto A',
+    'Produto B',
+    'Produto C',
+    'Produto D',
+    'Produto E',
+  ];
+  
+  // Função para gerar uma recomendação aleatória
+  function getRandomRecommendation() {
+    const randomIndex = Math.floor(Math.random() * products.length);
+    return products[randomIndex];
+  }
+  
+  // Exemplo de recomendação
+  const recommendedProduct = getRandomRecommendation();
+  
+  console.log('Produto recomendado:', recommendedProduct);
+  
